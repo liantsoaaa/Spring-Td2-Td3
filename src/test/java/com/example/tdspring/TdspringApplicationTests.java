7cadb82fe2058306309fa51491b510ae1292c0af
liantsoaaa <ramaharsionl@gmail.com>
2026-03-19 12:37:22 +0300

    first commit

---
+++ src/test/java/com/example/tdspring/TdspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tdspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
